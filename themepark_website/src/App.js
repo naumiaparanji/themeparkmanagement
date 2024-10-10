ba451d29f658814b55b7c07c4c82c74f9a4b11c6
YourCasualSenpai <45568983+YourCasualSenpai@users.noreply.github.com>
2024-10-10 14:41:17 -0500

    Added Nav Bar (Not Complete)
    Added a Navigation bar. Still needs to be Touched.
    Added a Temporary (Stock Image) as the Background. Will be changed.

--- themepark_website/src/App.js
+++ themepark_website/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import navbar from './navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <body>
+
+            <div className="navbar">
+                <section className="navbar-links">
+                    <a href="App.js">Home</a>
+                </section>
+                <section className="navbar-links">
+                    <a href="App.js">Rides</a>
+                </section>
+                <section className="navbar-links">
+                    <a href="App.js">Attractions</a>
+                </section>
+                <section className="navbar-links">
+                    <a href="App.js">Events</a>
+                </section>
+                <section className="navbar-links">
+                    <a href="App.js">About Us</a>
+                </section>
+            </div>
+
+            <div className="mainText">
+                <h1>7-Flags: Family Theme Park</h1>
+                <hr></hr>
+                <p>Located in Houston Texas, 7 Flags is a new thrilling Theme Park to spend time with the Family.</p>
+            </div>
+      </body>
   );
 }
 
