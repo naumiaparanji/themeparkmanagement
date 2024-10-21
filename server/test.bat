7c19a36b7e5b9159a2f2db1c9835b4deb02b6e6d
Ian Sloat <iansloat537@gmail.com>
2024-10-21 16:15:34 -0500

    Tweaked server deploy scripts

--- server/test.bat
+++ server/test.bat
@@ -2,7 +2,7 @@
 
 cd %~dp0
 
-set MYSQL_ADDR=192.168.1.84
+set MYSQL_ADDR=172.16.0.2
 set MYSQL_DB=themepark_db
 set MYSQL_KEYSTORE_DB=keystore_db
 set MYSQL_USER=admin
@@ -12,8 +12,5 @@ set APP_ADMIN_PASS=password
 set CLIENT_ORIGIN=http://localhost:3000
 set SERVER_PORT=8080
 
-echo Installing dependencies...
-call npm install libsodium-wrappers-sumo express express-session ejs mysql2 knex express-mysql-session cors --save
-
 echo Running server...
-node main.js
+npm install && node main.js
--- server/test.sh
+++ server/test.sh
@@ -14,7 +14,5 @@ export APP_ADMIN_PASS=password
 export CLIENT_ORIGIN=http://localhost:3000
 export SERVER_PORT=8080
 
-echo "Installing dependencies..."
-npm install libsodium-wrappers-sumo express express-session ejs mysql2 knex express-mysql-session cors --save # other packages can be added here
-
-node main.js
+echo Running server...
+npm install && node main.js
