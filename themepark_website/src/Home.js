6c31cd0a5b5c942e13cf259b131ce074b8d22f97
Naumi Shrivatsa Aparanji <naumisa9@gmail.com>
2024-10-21 03:35:36 -0500

    clearing up navbar format

--- themepark_website/src/Home.js
+++ themepark_website/src/Home.js
@@ -2,54 +2,64 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import BgVideo from './images/stock_footage_bg.mp4';
 import MainLogo from './images/flagslogo.png';
-import './CustomerAccount'
+import './CustomerAccount';
 import CustomerAccount from './CustomerAccount';
 
 function Home() {
+    // Common style for navbar links
+    const linkStyle = {
+        fontSize: '20px', // Change this to your desired size
+        fontWeight: 'bold', // Use 'fontWeight' for bold text
+        color: '#333', // Change this to your desired color
+        margin: '0 10px', // Optional: Add margin for spacing between links
+    };
+
     return (
         <div>
             <div className="notificationbar">
-            {/* Edit Notification Text */}
-            <h1 className="notificationtext">**WINTER SEASON PASSES AVAILABLE! LOGIN OR CREATE AN ACCOUNT FOR MORE INFORMATION.</h1>
-            <section className="loginbutton">
-                <a href="login" id="logintext">
-                    <CustomerAccount text="Log In" /> {/* Looks ugly but works */}
-                </a>
-            </section>
+                {/* Edit Notification Text */}
+                <h1 className="notificationtext">**WINTER SEASON PASSES AVAILABLE! LOGIN OR CREATE AN ACCOUNT FOR MORE INFORMATION.</h1>
+                <section className="loginbutton">
+                    <a href="login" id="logintext">
+                        <CustomerAccount text="Log In" /> {/* Looks ugly but works */}
+                    </a>
+                </section>
             </div>
+
             <div className="navbar">
                 <section className="navbar-links" id="navbar-logo">
                     <Link to="/">
-                        <img src={MainLogo} alt="logo" />
+                        <img src={MainLogo} alt="logo" style={{ width: '180px', height: 'auto' }} />
                     </Link>
                 </section>
                 <section className="navbar-links">
-                    <Link to="/">Home</Link>
+                    <Link to="/" style={linkStyle}>Home</Link>
                 </section>
                 <section className="navbar-links">
-                    <Link to="/rides">Rides</Link>
+                    <Link to="/rides" style={linkStyle}>Rides</Link>
                 </section>
                 <section className="navbar-links">
-                    <Link to="/attractions">Attractions</Link>
+                    <Link to="/attractions" style={linkStyle}>Attractions</Link>
                 </section>
                 <section className="navbar-links">
-                    <Link to="/events">Events</Link>
+                    <Link to="/events" style={linkStyle}>Events</Link>
                 </section>
                 <section className="navbar-links">
-                    <Link to="/about">About Us</Link>
+                    <Link to="/about" style={linkStyle}>About Us</Link>
                 </section>
             </div>
-
 
             <div className="mainText">
-                <video autoPlay loop muted id="bg-Video"><source src={BgVideo} type="video/mp4" /> </video>
-                <h2>A New Exhilirating Experience for that special day.</h2>
-                <hr></hr>
+                <video autoPlay loop muted id="bg-Video">
+                    <source src={BgVideo} type="video/mp4" />
+                </video>
+                <h2>A New Exhilarating Experience for that special day.</h2>
+                <hr />
                 <p>Explore our Website and book your next family outing with us!</p>
             </div>
-            <br></br><br></br><br></br><br></br><br></br>
+            <br /><br /><br /><br /><br /><br /><br />
         </div>
-  );
+    );
 }
 
 export default Home;
