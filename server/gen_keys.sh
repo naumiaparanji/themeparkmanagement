1a7d2ef49575c4308369c9bb23844e766c6c1949
Ian Sloat <iansloat537@gmail.com>
2024-10-21 16:30:18 -0500

    Update gen_keys.sh

--- server/gen_keys.sh
+++ server/gen_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-openssl genrsa -out server-key.pem 1024
+openssl genrsa -out server-key.pem 2048
 openssl req -new -key server-key.pem -out server-csr.pem
 openssl x509 -req -in server-csr.pem -signkey server-key.pem -out server-cert.pem
