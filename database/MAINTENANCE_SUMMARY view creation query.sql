e4a6331039292d706ce8aafe7160282b63b19a25
Jonathan <moulaf4@gmail.com>
2024-11-05 22:21:11 -0600

    Changed MAINTENANCE_SUMMARY view to account for Delete flag as sign that maintenance ticket ended

--- database/MAINTENANCE_SUMMARY view creation query.sql
+++ database/MAINTENANCE_SUMMARY view creation query.sql
@@ -5,29 +5,13 @@ SELECT
 	R.RideID AS Ride_ID,
     R.RideName AS Ride_Name,
     IF(M.MaintenanceID IS NULL, "No Ongoing Maintenance", M.MaintenanceID) AS Maintenance_ID,
-    IF(M.MaintenanceID IS NULL, "N/A",
-		IF((RS.RideID, MAX(RS.Created)) IN (
-			select
-				rs.RideID,
-				max(rs.Created)
-			from
-				RIDE_STATUS as rs
-			group by
-				rs.RideID
-		), "In Progress", "Completed")
-	) AS Status,
-    IF(M.Date IS NULL, "N/A", M.Date) AS Start_Date,
+    IF(M.RideID IS NULL, "N/A", IF(M.Deleted = 0, "In Progress", "Completed")) AS Status,
+    IF(M.Date IS NULL, "N/A", DATE(M.Date)) AS Start_Date,
     IF(M.Description IS NULL, "N/A", M.Description) AS Description
 FROM
 	RIDES AS R
 LEFT OUTER JOIN
-    RIDE_STATUS AS RS ON R.RideID = RS.RideID
-LEFT OUTER JOIN
     MAINTENANCE AS M ON M.RideID = R.RideID
-LEFT OUTER JOIN
-    M_STATUS AS MS ON (MS.RideStatusID = RS.RideStatusID AND MS.MaintenanceID = M.MaintenanceID)
-GROUP BY
-	M.MaintenanceID, R.RideID
 ORDER BY
 	R.RideID ASC,
     Status DESC,
