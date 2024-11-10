8500d7fe39f0e5d37fe535134db1b130ea651cfe
YourCasualSenpai <45568983+YourCasualSenpai@users.noreply.github.com>
2024-11-10 05:45:35 -0600

    Updating Style on Data Management to match other tabs
    Updated using styling similar/the same compared to Events and Attractions.

---
+++ themepark_website/src/Views/DataEdit/CustomerEditNav.js
@@ -0,0 +1,74 @@
+import { useContext, useState, useCallback, useEffect } from "react";
+import { Navbar, Container, Form, InputGroup, Button } from "react-bootstrap";
+import { EmployeeModalFormContainer, StaffManagerContext } from "../Staff";
+import { api } from "../../App";
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './DataManage.css';
+
+export function CustomerEditNav() {
+    const {search, setSearch} = useContext(StaffManagerContext);
+    const [createState, setCreateState] = useState(false);
+
+    const handleHide = useCallback(() => setCreateState(false), []);
+    const handleAddEvent = useCallback(() => setCreateState(true), []);
+
+    const {setShowForm} = useContext(StaffManagerContext);
+    const [returnedData, setReturnedData] = useState(['hello']); 
+    const [employee, SetEmployee] = useState({EmployeeID: 0, FirstName: '', LastName: '', Age: 0, Gender: ''})
+
+    const setInput = (e) => {
+        const {name, value} = e.target;
+        console.log(value);
+        if (name === 'EmployeeID' || name === 'Age') {
+            SetEmployee(prevState => ({
+                ...prevState,
+                [name]: parseInt(value)
+            }));
+            return;
+        }
+        SetEmployee(prevState => ({
+            ...prevState,
+            [name]: parseInt(value)
+        }));
+    }
+
+    const [data, setData] = useState([])
+
+    useEffect(()=> {
+    axios.get('http://localhost:8080')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+  }, [])
+
+    const fetchData = async () => {
+        console.log(employee);
+        const newData = await fetch('/api', {
+            method: 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        console.log(newData);
+        setReturnedData(newData.result)
+    }
+
+    return (
+        <>
+        <Navbar fixed="top">
+            <Container fluid>
+                <Form className="w-100">
+                    <InputGroup className="d-flex">
+                        <Form.Control placeholder="Search" className="m-0" value={search} />
+                        <Button onClick={() => setShowForm(true)} className="Create">Add User</Button>
+                    </InputGroup>
+                </Form>
+            </Container>
+        </Navbar>
+        <EmployeeModalFormContainer/> 
+        <hr/>
+        </>
+    );
+}
---
+++ themepark_website/src/Views/DataEdit/DataManage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './DataManage.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { EmployeeEditNav } from "./EmployeeEditNav";
+import { CustomerEditNav } from "./CustomerEditNav";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+
+export function DataManage() {
+    return (
+            <Container fluid>
+                <Tabs defaultActiveKey="Customer">
+                    <Tab eventKey="Employees" title="Employees">
+                    <Col>
+                        <Row>
+                            <EmployeeEditNav />
+                        </Row>
+                        <Row className="h-100 overflow-auto">
+                        </Row>
+                    </Col>
+                </Tab>
+
+                <Tab eventKey="Customer" title="Customer">
+                    <Col>
+                        <Row>
+                            <CustomerEditNav />
+                        </Row>
+                        <Row className="h-100 overflow-auto">
+                        </Row>
+                    </Col>
+                </Tab>
+            </Tabs>
+            </Container>
+    );
+}
+
+export default DataManage;
